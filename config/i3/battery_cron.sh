4e3cfc3963d78b29a0af9cbb552e0fbe00d8a155
Arjun Nair <arjunnair.mec@gmail.com>
2018-05-16 20:32:59 +0530

    Updated battery script functions

---
+++ config/i3/battery_cron.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BATTINFO=`acpi -b`
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " "` < 20% ]] ; then
+    DISPLAY=:0.0 /usr/bin/notify-send -u critical "LOW BATTERY" "$BATTINFO"
+fi
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " "` < 05% ]] ; then
+    DISPLAY=:0.0 /usr/bin/notify-send -u critical "VERY LOW BATTERY" "Shutting down in 3 mins"
+    shutdown +3
+fi
+if [[ `echo $BATTINFO | grep Charging` && `echo $BATTINFO | cut -f 4 -d " "` < 6% ]] ; then
+    DISPLAY=:0.0 /usr/bin/notify-send -u low "Charging" "Cancelling any shutdown"
+    shutdown --no-wal -c
+fi
+if [[ `echo $BATTINFO | grep Full` ]] ; then
+    DISPLAY=:0.0 /usr/bin/notify-send -u low "FULL BATTERY" "$BATTINFO"
+fi
