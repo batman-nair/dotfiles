a6ef015de53869356a92fbdbed0ecc8e7267f339
Arjun Nair <arjunnair.mec@gmail.com>
2018-05-17 23:02:33 +0530

    Small code correction

--- config/i3/battery_cron.sh
+++ config/i3/battery_cron.sh
@@ -8,7 +8,7 @@ if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " "` 
     DISPLAY=:0.0 /usr/bin/notify-send -u critical "VERY LOW BATTERY" "Shutting down in 3 mins"
     shutdown +3
 fi
-if [[ `echo $BATTINFO | grep Charging` && `echo $BATTINFO | cut -f 4 -d " "` < 6% ]] ; then
+if [[ `echo $BATTINFO | grep Charging` && `echo $BATTINFO | cut -f 4 -d " "` < 06% ]] ; then
     DISPLAY=:0.0 /usr/bin/notify-send -u low "Charging" "Cancelling any shutdown"
     shutdown --no-wal -c
 fi
